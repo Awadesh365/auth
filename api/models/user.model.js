9102d63179a90de0e850e211beed351858c536f3
Awadesh <36awadesh@gmail.com>
2024-11-05 10:57:29 +0530

    add comments to user model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,6 +16,8 @@ const userSchema = new mongoose.Schema({
         required: true,
     }
 }, { timestamps: true });
+// each user will have a seperate thing as time or creation and time of updation, and the timestamps: true does this for us.
+
 
 const User = mongoose.model("User", userSchema);
 
