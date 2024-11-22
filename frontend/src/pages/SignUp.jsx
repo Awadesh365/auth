4565c7bd17d30735f2d332fdd442cb188da76cff
Awadesh <36awadesh@gmail.com>
2024-11-22 15:37:01 +0530

    Complete signup page functionality

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,84 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+    setError(null);
+
+    try {
+      const res = await axios.post("/api/auth/signup", formData);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(error.message || "Something went wrong.");
+    }
+  };
+
   return (
-    <div>
-      <div className="bg-sky-100 flex justify-center items-center h-screen">
-        <div className="w-1/2 h-screen hidden lg:block">
-          <img
-            src="https://img.freepik.com/fotos-premium/imagen-fondo_910766-187.jpg?w=826"
-            alt="Placeholder Image"
-            className="object-cover w-full h-full"
-          />
-        </div>
+    <div className="bg-sky-100 flex justify-center items-center h-screen">
+      <div className="w-1/2 h-screen hidden lg:block">
+        <img
+          src="https://img.freepik.com/fotos-premium/imagen-fondo_910766-187.jpg?w=826"
+          alt="Placeholder"
+          className="object-cover w-full h-full"
+        />
+      </div>
 
-        <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
-          <h1 className="text-2xl font-semibold mb-4">SignUp</h1>
-          <form action="#" method="POST">
-            <div className="mb-4 bg-sky-100">
-              <label htmlFor="username" className="block text-gray-600">
-                Username
-              </label>
-              <input
-                type="text"
-                id="username"
-                name="username"
-                className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
-                autocomplete="off"
-              />
-            </div>
+      <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
+        <h1 className="text-2xl font-semibold mb-4">SignUp</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4 bg-sky-100">
+            <label htmlFor="username" className="block text-gray-600">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+              onChange={handleChange}
+            />
+          </div>
 
-            <div className="mb-4 bg-sky-100">
-              <label htmlFor="username" className="block text-gray-600">
-                email
-              </label>
-              <input
-                type="text"
-                id="username"
-                name="username"
-                className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
-                autocomplete="off"
-              />
-            </div>
+          <div className="mb-4 bg-sky-100">
+            <label htmlFor="email" className="block text-gray-600">
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+              onChange={handleChange}
+            />
+          </div>
 
-            <div className="mb-4">
-              <label htmlFor="password" className="block text-gray-800">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
-                autocomplete="off"
-              />
-            </div>
-            <button
-              type="submit"
-              className="bg-red-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full"
-            >
-              SignUp
-            </button>
-          </form>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-800">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+              onChange={handleChange}
+            />
+          </div>
+
+          {error && <p className="text-red-500 text-sm">{error}</p>}
 
           <button
-            className="bg-blue-800 mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
-            type="button"
+            type="submit"
+            className={`bg-red-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full ${
+              loading ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+            disabled={loading}
           >
-            <span class="text-sm text-white tracking-wider">
-              Sign-In with Google
-            </span>
+            {loading ? "Submitting..." : "SignUp"}
           </button>
-          <div>
-            Have an Account? .
-            <Link
-              className="hover:underline mt-6 text-green-500 text-center"
-              to="/sign-in"
-            >
-              Sign-In here
-            </Link>
-          </div>
+        </form>
+
+        <button
+          className="bg-blue-800 mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white"
+          onClick={() =>
+            alert("Google Sign-In functionality not yet implemented.")
+          }
+        >
+          <span className="text-sm text-white tracking-wider">
+            Sign-In with Google
+          </span>
+        </button>
+
+        <div>
+          Have an Account?{" "}
+          <Link
+            className="hover:underline mt-6 text-green-500 text-center"
+            to="/sign-in"
+          >
+            Sign-In here
+          </Link>
         </div>
       </div>
     </div>
