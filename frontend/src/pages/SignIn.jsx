28eb95a2a129de4a31bca32854d30d98bed51ddd
Awadesh <36awadesh@gmail.com>
2024-11-25 11:42:13 +0530

    Complete SignIn Page Functionality.

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,71 +1,109 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
-export default function SignIn() {
+function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(false);
+  const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+
   const navigate = useNavigate();
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    setLoading(true);
+    setError(null);
+
     try {
-      setLoading(true);
-      setError(false);
-      const res = await fetch("/api/auth/signin", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
-      const data = await res.json();
+      const res = await axios.post("/api/auth/signin", formData);
       setLoading(false);
-      if (data.success === false) {
-        setError(true);
-        return;
-      }
       navigate("/");
     } catch (error) {
       setLoading(false);
-      setError(true);
+      setError(error.message || "Something went wrong.");
     }
   };
+
   return (
-    <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <input
-          type="email"
-          placeholder="Email"
-          id="email"
-          className="bg-slate-100 p-3 rounded-lg"
-          onChange={handleChange}
+    <div className="bg-sky-100 flex justify-center items-center h-screen">
+      <div className="w-1/2 h-screen hidden lg:block">
+        <img
+          src="https://img.freepik.com/fotos-premium/imagen-fondo_910766-187.jpg?w=826"
+          alt="Placeholder"
+          className="object-cover w-full h-full"
         />
-        <input
-          type="password"
-          placeholder="Password"
-          id="password"
-          className="bg-slate-100 p-3 rounded-lg"
-          onChange={handleChange}
-        />
-        <button
-          disabled={loading}
-          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
-        >
-          {loading ? "Loading..." : "Sign In"}
-        </button>
-      </form>
-      <div className="flex gap-2 mt-5">
-        <p>Dont Have an account?</p>
-        <Link to="/sign-up">
-          <span className="text-blue-500">Sign up</span>
-        </Link>
       </div>
-      <p className="text-red-700 mt-5">{error && "Something went wrong!"}</p>
+
+      <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
+        <h1 className="text-2xl font-semibold mb-4">SignIn</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4 bg-sky-100">
+            <label htmlFor="email" className="block text-gray-600">
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-800">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+              onChange={handleChange}
+            />
+          </div>
+
+          {error && <p className="text-red-500 text-sm">{error}</p>}
+
+          <button
+            type="submit"
+            className={`bg-red-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full ${
+              loading ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+            disabled={loading}
+          >
+            {loading ? "Submitting..." : "SignIn"}
+          </button>
+        </form>
+
+        <button
+          className="bg-blue-800 mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white"
+          onClick={() =>
+            alert("Google Sign-In functionality not yet implemented.")
+          }
+        >
+          <span className="text-sm text-white tracking-wider">
+            Sign-In with Google
+          </span>
+        </button>
+
+        <div>
+          Dont Have an Account?{" "}
+          <Link
+            className="hover:underline mt-6 text-green-500 text-center"
+            to="/sign-up"
+          >
+            Sign-Up here
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
+
+export default SignIn;
